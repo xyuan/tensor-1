a5202a55b1e15d6f2fc1443e73b420d46416afbf
Sun Rongyang <sun-rongyang@outlook.com>
2020-02-25 14:15:22 +0800

    Comment more code to start the framework refactor

--- src/qn.cc
+++ src/qn.cc
@@ -21,102 +21,102 @@
 namespace gqten {
 
 
-QN::QN(void) { hash_ = CalcHash(); }
+//QN::QN(void) { hash_ = CalcHash(); }
 
 
-QN::QN(const std::vector<QNNameVal> &nm_vals) {
-  for (auto &nm_val : nm_vals) {
-    values_.push_back(nm_val.val);
-  }
-  hash_ = CalcHash();
-}
+//QN::QN(const std::vector<QNNameVal> &nm_vals) {
+  //for (auto &nm_val : nm_vals) {
+    //values_.push_back(nm_val.val);
+  //}
+  //hash_ = CalcHash();
+//}
 
 
-QN::QN(const std::vector<long> &qn_vals) : values_(qn_vals) {
-  hash_ = CalcHash();
-}
+//QN::QN(const std::vector<long> &qn_vals) : values_(qn_vals) {
+  //hash_ = CalcHash();
+//}
 
 
-QN::QN(const QN &qn) : values_(qn.values_), hash_(qn.hash_) {}
+//QN::QN(const QN &qn) : values_(qn.values_), hash_(qn.hash_) {}
 
 
-QN &QN::operator=(const QN &rhs) {
-  values_ = rhs.values_;
-  hash_ = rhs.hash_;
-  return *this;
-}
+//QN &QN::operator=(const QN &rhs) {
+  //values_ = rhs.values_;
+  //hash_ = rhs.hash_;
+  //return *this;
+//}
 
 
-std::size_t QN::Hash(void) const { return hash_; }
+//std::size_t QN::Hash(void) const { return hash_; }
 
 
-std::size_t QN::CalcHash(void) const {
-  if (values_.size() == 0) {
-    return 0; 
-  } else {
-    return VecStdTypeHasher(values_);
-  }
-}
+//std::size_t QN::CalcHash(void) const {
+  //if (values_.size() == 0) {
+    //return 0; 
+  //} else {
+    //return VecStdTypeHasher(values_);
+  //}
+//}
 
 
-QN QN::operator-(void) const {
-  auto qn_vals_size = this->values_.size();
-  std::vector<long> new_qn_vals(qn_vals_size);
-  for (std::size_t i = 0; i < qn_vals_size; ++i) {
-    new_qn_vals[i] = - this->values_[i];
-  }
-  return QN(new_qn_vals);
-}
+//QN QN::operator-(void) const {
+  //auto qn_vals_size = this->values_.size();
+  //std::vector<long> new_qn_vals(qn_vals_size);
+  //for (std::size_t i = 0; i < qn_vals_size; ++i) {
+    //new_qn_vals[i] = - this->values_[i];
+  //}
+  //return QN(new_qn_vals);
+//}
 
 
-QN &QN::operator+=(const QN &rhs) {
-  auto qn_vals_size = this->values_.size();
-  assert(qn_vals_size == rhs.values_.size());
-  for (std::size_t i = 0; i < qn_vals_size; ++i) {
-    this->values_[i] += rhs.values_[i];
-  }
-  hash_ = CalcHash();
-  return *this;
-}
+//QN &QN::operator+=(const QN &rhs) {
+  //auto qn_vals_size = this->values_.size();
+  //assert(qn_vals_size == rhs.values_.size());
+  //for (std::size_t i = 0; i < qn_vals_size; ++i) {
+    //this->values_[i] += rhs.values_[i];
+  //}
+  //hash_ = CalcHash();
+  //return *this;
+//}
 
 
-QN operator+(const QN &lhs, const QN &rhs) {
-  QN sum(lhs);
-  sum += rhs;
-  return sum;
-}
+//QN operator+(const QN &lhs, const QN &rhs) {
+  //QN sum(lhs);
+  //sum += rhs;
+  //return sum;
+//}
 
 
-QN operator-(const QN &lhs, const QN &rhs) {
-  return lhs + (-rhs);
-}
+//QN operator-(const QN &lhs, const QN &rhs) {
+  //return lhs + (-rhs);
+//}
 
 
-bool operator==(const QN &lhs, const QN &rhs) {
-  return lhs.Hash() == rhs.Hash();
-}
+//bool operator==(const QN &lhs, const QN &rhs) {
+  //return lhs.Hash() == rhs.Hash();
+//}
 
 
-bool operator!=(const QN &lhs, const QN &rhs) {
-  return !(lhs == rhs);
-}
+//bool operator!=(const QN &lhs, const QN &rhs) {
+  //return !(lhs == rhs);
+//}
 
 
-std::ifstream &bfread(std::ifstream &ifs, QN &qn) {
-  long qn_vals_num;
-  ifs >> qn_vals_num;
-  qn.values_ = std::vector<long>(qn_vals_num);
-  for (auto &value : qn.values_) { ifs >> value; }
-  ifs >> qn.hash_;
-  return ifs;
-}
+//std::ifstream &bfread(std::ifstream &ifs, QN &qn) {
+  //long qn_vals_num;
+  //ifs >> qn_vals_num;
+  //qn.values_ = std::vector<long>(qn_vals_num);
+  //for (auto &value : qn.values_) { ifs >> value; }
+  //ifs >> qn.hash_;
+  //return ifs;
+//}
 
 
-std::ofstream &bfwrite(std::ofstream &ofs, const QN &qn) {
-  long qn_vals_num = qn.values_.size();
-  ofs << qn_vals_num << std::endl;
-  for (auto &value : qn.values_) { ofs << value << std::endl; }
-  ofs << qn.hash_ << std::endl;
-  return ofs;
-}
+//std::ofstream &bfwrite(std::ofstream &ofs, const QN &qn) {
+  //long qn_vals_num = qn.values_.size();
+  //ofs << qn_vals_num << std::endl;
+  //for (auto &value : qn.values_) { ofs << value << std::endl; }
+  //ofs << qn.hash_ << std::endl;
+  //return ofs;
+//}
 } /* gqten */ 
